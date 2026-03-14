03a92efaecfdcf4608e73427c90d555e7d27f9c5
ravshantorayev105-alt <ravshantorayev105@gmail.com>
2026-03-14 09:46:08 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = "BOT_TOKEN"
+TOKEN = "8695000572:AAGJndNz6mKP-_kjmVvFRTbCBkohX2gRR40"
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
